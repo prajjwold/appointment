33febc8d9dcd55fcc6c5736a356fb78377ffbe96
Prajjwol Dandekhya <dprajjwol@gmail.com>
2020-09-13 20:12:13 -0700

    Add API documentation

--- src/main/java/com/notablehealth/appointment/controllers/AppointmentsController.java
+++ src/main/java/com/notablehealth/appointment/controllers/AppointmentsController.java
@@ -1,82 +1,76 @@
 package com.notablehealth.appointment.controllers;
 
+import com.notablehealth.appointment.models.Appointment;
+import com.notablehealth.appointment.repositories.AppointmentRepository;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
-
-import com.notablehealth.appointment.models.Appointment;
-import com.notablehealth.appointment.repositories.AppointmentRepository;
-
 @RestController
 @RequestMapping("/api/v1/appointments")
 public class AppointmentsController {
-	@Autowired
-	private AppointmentRepository appointmentRepository;
+    @Autowired
+    private AppointmentRepository appointmentRepository;
 
-	@GetMapping
-	public List<Appointment> list() {
-		return appointmentRepository.findAll();
-	}
+    @ApiOperation(value = "Get the list of all doctors appointments", notes = "This api returns all the list of doctors appointment so no paging is facilitated")
+    @GetMapping
+    public List<Appointment> list() {
+        return this.appointmentRepository.findAll();
+    }
 
-	@GetMapping("/{id}")
-	public Appointment get(@PathVariable("id") long id) {
-		return appointmentRepository.getOne(id);
-	}
+    @GetMapping("/{id}")
+    public Appointment get(@PathVariable("id") long id) {
+        return this.appointmentRepository.getOne(id);
+    }
 
-	@PostMapping
-	@ResponseStatus(HttpStatus.OK)
-	public void create(@RequestBody Appointment appointment) {
+    @PostMapping
+    @ResponseStatus(HttpStatus.OK)
+    public void create(@RequestBody Appointment appointment) {
 
-		Date date = appointment.getDate();
-		if (!isValidTime(date)) {
-			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid Time");
-		}
+        Date date = appointment.getDate();
+        if (!this.isValidTime(date)) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid Time");
+        }
 
-		long doctorId = appointment.getDoctor().getId();
-		long count = appointmentRepository.countByDoctorIdAndDateEquals(doctorId, date);
+        long doctorId = appointment.getDoctor().getId();
+        long count = this.appointmentRepository.countByDoctorIdAndDateEquals(doctorId, date);
 
-		if (count >= 3) {
-			throw new ResponseStatusException(HttpStatus.CONFLICT, "Appointment not available for a doctor");
-		}
+        if (count >= 3) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Appointment not available for a doctor");
+        }
 
-		appointmentRepository.save(appointment);
-	}
+        this.appointmentRepository.save(appointment);
+    }
 
-	@DeleteMapping("/{id}")
-	@ResponseStatus(HttpStatus.OK)
-	public void delete(@PathVariable("id") long id) {
-		appointmentRepository.deleteById(id);
-	}
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void delete(@PathVariable("id") long id) {
+        this.appointmentRepository.deleteById(id);
+    }
 
-	@GetMapping("/doctors/{doctorId}/date/{date}")
+    @ApiOperation(value = "Get the list of all appointments for a doctor at a specific date time")
+    @GetMapping("/doctors/{doctorId}/date/{date}")
+    public List<Appointment> list(@PathVariable("doctorId") long doctorId, @PathVariable("date") String date) {
+        DateFormat format = new SimpleDateFormat("MM-dd-yyyy hh:mm", Locale.ENGLISH);
+        Date appointmentDate;
+        try {
+            appointmentDate = format.parse(date);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid Date format");
+        }
+        return this.appointmentRepository.findAllByDoctorIdAndDate(doctorId, appointmentDate);
+    }
 
-	public List<Appointment> list(@PathVariable("doctorId") long doctorId, @PathVariable("date") String date) {
-		DateFormat format = new SimpleDateFormat("MM-dd-yyyy hh:mm", Locale.ENGLISH);
-		Date appointmentDate;
-		try {
-			appointmentDate = format.parse(date);
-		} catch (Exception e) {
-			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid Date format");
-		}
-		return appointmentRepository.findAllByDoctorIdAndDate(doctorId, appointmentDate);
-	}
-
-	private boolean isValidTime(Date date) {
-		int minutes = date.getMinutes();
-		return (minutes == 0 || minutes == 15 || minutes == 30 || minutes == 45);
-	}
+    private boolean isValidTime(Date date) {
+        int minutes = date.getMinutes();
+        return (minutes == 0 || minutes == 15 || minutes == 30 || minutes == 45);
+    }
 }
--- src/main/java/com/notablehealth/appointment/swagger/SwaggerConfig.java
+++ src/main/java/com/notablehealth/appointment/swagger/SwaggerConfig.java
@@ -2,21 +2,38 @@ package com.notablehealth.appointment.swagger;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
+//@EnableSwagger2WebMvc
+//@Import({SpringDataRestConfiguration.class, BeanValidatorPluginsConfiguration.class})
 public class SwaggerConfig {
-	@Bean
-	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2).select()
-				// .apis(RequestHandlerSelectors.any())
-				.apis(RequestHandlerSelectors.basePackage("com.notablehealth.appointment")).paths(PathSelectors.any())
-				.build();
-	}
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                //.apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.notablehealth.appointment"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(this.getApiInfo());
+    }
+    
+    private ApiInfo getApiInfo() {
+        return new ApiInfoBuilder()
+                .title("Doctors Appointment API")
+                .version("1.0")
+                .description("A basic calendar API for managing a list of physicians and a list of appointments for the selected physician")
+                .contact(new Contact("Prajjwol Dandekhya", "http://pdandekhya.com", "dprajjwol@gmail.com"))
+                .license("Apache License Version 2.0")
+                .build();
+    }
 }
